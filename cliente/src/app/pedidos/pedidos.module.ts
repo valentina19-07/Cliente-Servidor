760beceb57cefd76d3d5fc1f184e74f45d4bbc4f
juan fernando vargas <juan.vargas03@uceva.edu.co>
2026-09-25 18:02:08 -0500

    feat(client): estructura módulo pedidos

---
+++ cliente/src/app/pedidos/pedidos.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PedidosRoutingModule } from './pedidos-routing.module';
+import { PedidosComponent } from './pedidos.component';
+
+@NgModule({
+  declarations: [PedidosComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    PedidosRoutingModule
+  ],
+  exports: [PedidosComponent]
+})
+export class PedidosModule { }
