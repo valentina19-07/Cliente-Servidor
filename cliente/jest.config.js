98f1373e6c0d29194d227e077cc127d966849f85
JuanFernando <juan.vargas03@uceva.edu.co>
2026-09-25 18:08:01 -0500

    Revert "Feature/client modulos"

--- cliente/jest.config.js
+++
@@ -1,5 +0,0 @@
-﻿module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-};
